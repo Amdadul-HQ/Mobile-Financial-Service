f0ee6ce99656a0da7aa794fafe851efaa365d45a
AmdadulHQ <rimonamdadul301@gmail.com>
2024-07-25 10:39:10 +0600

    adding payment history

--- client_side/src/Pages/PaymentHistory/PaymentHistory.jsx
+++ client_side/src/Pages/PaymentHistory/PaymentHistory.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import SendMoneyHistory from "./SendMoneyHistory";
+
 
 const PaymentHistory = () => {
+
     return (
         <section className='my-10 px-4'>
             <div className='bg-slate-200 shadow-slate-200 shadow-xl rounded-2xl p-4'>
                 <h1 className='text-lg font-medium text-center leading-6 text-gray-900'>Payment History</h1>
             </div>
+            <SendMoneyHistory/>
         </section>
     );
 };
